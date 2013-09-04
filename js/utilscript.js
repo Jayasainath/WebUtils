b9465d0e2cfd717359bffe97a8059c05b1dfcbfc
Test Prodigy <testprodigy.currentuser@gmail.com>
2013-09-04 09:08:51 +0530

    Added a part of ouput

--- js/utilscript.js
+++ js/utilscript.js
@@ -1,5 +1,5 @@
 DateUtils = function(){
-	var _t = this, inputDate, inputMonth, inputYear;
+	var _t = this, inputDay, inputDate, inputMonth, inputYear;
 	var dateObject = new Date();
 
 	var monthNamesShort = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
@@ -8,6 +8,11 @@ DateUtils = function(){
 	var dayNamesShort = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 	var dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 	
+	_t.getInputDay = function(){ return _t.inputDay; };
+	_t.setInputDay = function(value){
+		if(value) _t.inputDay = value;
+	};
+
 	_t.getInputDate = function(){ return _t.inputDate; };
 	_t.setInputDate = function(value){
 		if(value) _t.inputDate = value;
@@ -30,14 +35,20 @@ DateUtils = function(){
 
 	_t.format = function(value, outputFormat, inputFormat){
 		if(value && typeof value === "string"){
-			var splitter = /(\-|\/|\\|\.|\:|\,|\_|\*|\^|\#)/g;
+			var splitter = /(\-|\/|\\|\.|\:|\,|\_|\*|\^|\#|\s)/g;
 			var splitArray = inputFormat.split(splitter);
 			var splitArrayLength = splitArray.length, i = 0;
 			
 			for (; i < splitArrayLength; i+=2){
 				var split_value = value.split(splitter)[i];
-				if(splitArray[i].match(/(?:d|dd)/ig)){
+				if(splitArray[i].match(/(?:d|dd)/g)){
 					if(!isNaN(parseInt(split_value))) _t.setInputDate(parseInt(split_value));
+				}else if(splitArray[i].match(/(?:D|DD)/g)){
+					if(!isNaN(parseInt(split_value))) _t.setInputDay(parseInt(split_value));
+					
+					// check if the day name matches with the date, month and year combination
+					// if there's a match, create a date object, else throw an error
+
 				}else if(splitArray[i].match(/(?:m|mm)/g)){
 					if(!isNaN(parseInt(split_value))) _t.setInputMonth(parseInt(split_value)-1);
 				}else if(splitArray[i].match(/(?:M|MM)/g)){
@@ -54,10 +65,39 @@ DateUtils = function(){
 				}
 			}
 			_t.createDateObjectFor();
+			console.log("Date Object: "+_t.getDateObject());
 		}else if(value && typeof value == "object"){
 
 		}
 
+	};
+
+	_t.formatOutput = function(outputFormat){
+		if(value && typeof value === "string"){
+			var dateStr = "", dateObject = _t.getDateObject();
+			var splitter = /(\-|\/|\\|\.|\:|\,|\_|\*|\^|\#|\s)/g;
+			var splitArray = outputFormat.split(splitter);
+			var splitArrayLength = splitArray.length, i = 0;
+			for (; i < splitArrayLength; i+=2){
+				if(splitArray[i].match(/(?:d|dd)/g)){
+					var date = dateObject.getDate().toString();
+					if(splitArray[i].length > 1){
+						date = date.length < 2 ? "0" + date : date;
+					}
+					dateStr += date;
+				}else if(splitArray[i].match(/(?:D|DD)/g)){
+					var day = _t.getDateObject().getDay();
+					day = splitArray[i].length > 1 ? dayNames[day] : dayNamesShort[day];
+					dateStr += day;
+				}else if(splitArray[i].match(/(?:m|mm)/g)){
+					
+				}else if(splitArray[i].match(/(?:M|MM)/g)){
+					
+				}else if(splitArray[i].match(/(?:yy|yyyy)/ig)){
+					
+				}
+			}
+		}
 	};
 
 	_t.createDateObjectFor = function(){
@@ -75,7 +115,7 @@ DateUtils = function(){
 
 (function(){
 	var df = DateUtils;
-	df.format("dec-11-13", "mm-dd-yy", "M/dd/yy");
+	df.format("dec-11-13", "D-mm-dd-yy", "M,dd-yy");
 })();
 
 
@@ -83,37 +123,37 @@ DateUtils = function(){
 /*
 
 ==============================================================================================================================================
-INPUT FORMAT
-============
+INPUT FORMAT (S)
+================
 
-dd/mm/yy
-dd/mm/yyyy
-d/m/yy
+['d/m/yy', 'd/m/yyyy', 'd/mm/yy', 'd/mm/yyyy', 'd/M/yy', 'd/M/yyyy', 'd/MM/yy', 'd/MM/yyyy']
+['dd/m/yy', 'dd/m/yyyy', 'dd/mm/yy', 'dd/mm/yyyy', 'dd/M/yy', 'dd/M/yyyy', 'dd/MM/yy', 'dd/MM/yyyy']
+['d/yy/m', 'd/yyyy/m', 'd/yy/mm', 'd/yyyy/mm', 'd/yy/M', 'd/yyyy/M', 'd/yy/MM', 'd/yyyy/MM']
+['dd/yy/m', 'dd/yyyy/m', 'dd/yy/mm', 'dd/yyyy/mm', 'dd/yy/M', 'dd/yyyy/M', 'dd/yy/MM', 'dd/yyyy/MM']
 
-d/M/yy
-dd/M/yy
-dd/M/yyyy
-d/MM/yy
-dd/MM/yy
-dd/MM/yyyy
+**********
 
-mm/dd/yy
-mm/dd/yyyy
-m/d/yy
+['m/d/yy', 'm/d/yyyy', 'mm/d/yy', 'mm/d/yyyy', 'M/d/yy', 'M/d/yyyy', 'MM/d/yy', 'MM/d/yyyy']
+['m/dd/yy', 'm/dd/yyyy', 'mm/dd/yy', 'mm/dd/yyyy', 'M/dd/yy', 'M/dd/yyyy', 'MM/dd/yy', 'MM/dd/yyyy']
+['m/yy/d', 'm/yyyy/d', 'mm/yy/d', 'mm/yyyy/d', 'M/yy/d', 'M/yyyy/d', 'MM/yy/d', 'MM/yyyy/d']
+['m/yy/dd', 'm/yyyy/dd', 'mm/yy/dd', 'mm/yyyy/dd', 'M/yy/dd', 'M/yyyy/dd', 'MM/yy/dd', 'MM/yyyy/dd']
 
-yy/m/d
-yy/mm/dd
-yyyy/mm/dd
+**********
 
-yy/d/m
-yy/dd/mm
-yyyy/dd/mm
+['yy/m/d', 'yy/mm/d', 'yy/m/dd', 'yy/mm/dd', 'yy/M/d', 'yy/MM/d', 'yy/M/dd', 'yy/MM/dd']
+[y'yyy/m/d', 'yyyy/mm/d', 'yyyy/m/dd', 'yyyy/mm/dd', 'yyyy/M/d', 'yyyy/MM/d', 'yyyy/M/dd', 'yyyy/MM/dd']
+['yy/d/m', 'yy/d/mm', 'yy/dd/m', 'yy/dd/mm', 'yy/d/M', 'yy/d/MM', 'yy/dd/M', 'yy/dd/MM']
+[y'yyy/d/m', 'yyyy/d/mm', 'yyyy/dd/m', 'yyyy/dd/mm', 'yyyy/d/M', 'yyyy/d/MM', 'yyyy/dd/M', 'yyyy/dd/MM']
 
-The user can use his choice of delimeters like (\/.,;*:#^|)
+**********
+
+The user can use his choice of delimeters like (\/., ;*:#^|)
 
 ==============================================================================================================================================
 OUTUT FORMAT
 ============
+
+Can be any of the above input formats in additon to the day name (D/DD)
 
 
 
